d2642e1cf9be3a871d0808e1d99e2dca6c298d0d
Nikolai Shepelev <venomkol@mail.ru>
2018-01-14 18:45:32 +0700

    Дополнена основная библиотека классов

--- Airport/Airport.cs
+++ Airport/Airport.cs
@@ -162,7 +162,7 @@ namespace Airport
         // Конструкторы
         public Passenger() : base()
         {
-            Age = 200;
+            Age = 199;
         }
         public Passenger(String FullName, Int32 Age) : base(FullName)
         {
@@ -182,7 +182,7 @@ namespace Airport
             }
             set
             {
-                if (value >= 0 && value <= 200)
+                if (value >= 0 && value < 200)
                     Age = value;
                 else
                     throw new ArgumentOutOfRangeException("Age", "Возраст пассажира должен быть не отрицательным и менее 200");
@@ -229,11 +229,133 @@ namespace Airport
     // Класс "Самолет"
     class Aircraft
     {
-        private Pilot СhiefPilot;
-        private Pilot SecondPilot;
-        private String Model;
-        private String SerialNumber;
+        private Pilot СhiefPilot;       // Первый пилот
+        private String Model;           // Модель самолета
+        private String SerialNumber;    // Номер самолета
+        private Int32 Seats;            // Количество мест в самолете
 
+        // Конструкторы
+        public Aircraft()
+        {
+            СhiefPilot = new Pilot();
+            Model = "XXXX";
+            SerialNumber = "XXXXX";
+            Seats = 0;
+        }
+        public Aircraft(Pilot СhiefPilot, Pilot SecondPilot, String Model, String SerialNumber, Int32 Seats)
+        {
+            this.СhiefPilot = СhiefPilot;
+            this.Model = Model;
+            this.SerialNumber = SerialNumber;
+            this.Seats = Seats;
+        }
+
+        // Свойства
+        public Pilot AircraftСhiefPilot
+        {
+            get
+            {
+                return СhiefPilot;
+            }
+            set
+            {
+                if (value != null)
+                    СhiefPilot = value;
+                else
+                    СhiefPilot = new Pilot();
+            }
+        }
+        public String AircraftModel
+        {
+            get
+            {
+                return Model;
+            }
+            set
+            {
+                if (value.Length > 0 && value.Length < 16)
+                    Model = value;
+                else
+                    throw new ArgumentOutOfRangeException("Model", "Модель самолета должна быть длиной не более 16 символов и не пустым");
+            }
+        }
+        public String AircraftSerialNumber
+        {
+            get
+            {
+                return SerialNumber;
+            }
+            set
+            {
+                if (value.Length > 0 && value.Length < 16)
+                    SerialNumber = value;
+                else
+                    throw new ArgumentOutOfRangeException("SerialNumber", "Номер самолета должен быть длиной не более 16 символов и не пустым");
+            }
+        }
+        public Int32 AircraftSeats
+        {
+            get
+            {
+                return Seats;
+            }
+            set
+            {
+                if (value >= 0 && value < 1000)
+                    Seats = value;
+                else
+                    throw new ArgumentOutOfRangeException("Seats", "Количетво мест в самолете должно быть не отрицательным и менее 1000");
+            }
+        }
+    }
+
+    // Класс "Билет"
+    class Ticket
+    {
+        private Int32 Seat;     // Место
+        private Double Price;   // Цена
+
+        // Конструкторы
+        Ticket()
+        {
+            Seat = 0;
+            Price = 0.0;
+        }
+        Ticket(Int32 Seat, Double Price)
+        {
+            this.Seat = Seat;
+            this.Price = Price;
+        }
+
+        // Свойства
+        public Int32 TicketSeat
+        {
+            get
+            {
+                return Seat;
+            }
+            set
+            {
+                if (value >= 0 && value < 1000)
+                    Seat = value;
+                else
+                    throw new ArgumentOutOfRangeException("Seat", "Номер места в самолете должен быть не отрицательным и менее 1000");
+            }
+        }
+        public Double TicketPrice
+        {
+            get
+            {
+                return Price;
+            }
+            set
+            {
+                if (value >= 0.0 && value < Double.MaxValue)
+                    Price = value;
+                else
+                    throw new ArgumentOutOfRangeException("Price", "Цена билета должна быть не отрицательной и менее " + Convert.ToString(Double.MaxValue));
+            }
+        }
     }
 
     // Класс "Рейс"
